c6a1adcc0c76deeb0a07aa54de1cfa42c5cb146f
davelorino <davelorino@gmail.com>
2019-08-18 21:45:17 +1000

    Added single user search

--- controllers/user.js
+++ controllers/user.js
@@ -29,5 +29,9 @@ exports.allUsers = (req, res) => {
       });
     }
     res.json({ users });
-  });
+  }).select("name email updated created");
+};
+
+exports.getUser = (req, res) => {
+  return res.json(req.profile);
 };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 // routes user
 
 const express = require("express");
-const {userById, allUsers} = require("../controllers/user");
+const {userById, allUsers, getUser} = require("../controllers/user");
 const router = express.Router();
 
 router.get("/users", allUsers);
+router.get("/user/:userId", getUser);
 
 // any route containing :userId, our app will first execute userById
 router.param("userId", userById);
