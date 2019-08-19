0a1c606d4f488b63145219ee9222aacd124938f6
davelorino <davelorino@gmail.com>
2019-08-20 01:13:01 +1000

    Adding deleteUser Function

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 // routes user
 
 const express = require("express");
-const {userById, allUsers, getUser, updateUser} = require("../controllers/user");
+const {userById, allUsers, getUser, updateUser, deleteUser} = require("../controllers/user");
 const router = express.Router();
 const {requireSignin} = require("../controllers/auth");
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
 router.put("/user/:userId", requireSignin, updateUser);
+router.delete("/user/:userId", requireSignin, deleteUser);
 
 // any route containing :userId, our app will first execute userById
 router.param("userId", userById);
