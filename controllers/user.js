5f2fa08e4b4c762b9fa1e213123584951218a5a1
davelorino <davelorino@gmail.com>
2019-08-20 00:56:28 +1000

    Adding updateUser Function

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 // controllers user
 const User = require("../models/user");
+const _ = require("lodash");
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -33,5 +34,26 @@ exports.allUsers = (req, res) => {
 };
 
 exports.getUser = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
   return res.json(req.profile);
 };
+
+exports.updateUser = (req, res, next) => {
+  let user = req.profile;
+  user = _.extend(user, req.body); // change the user data structure with the req.body
+  user.updated = Date.now();
+  user.save((err) => {
+    if(err) {
+      return res.status(400).json({
+        error: "You are not authorised to update this user."
+      });
+    }
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    res.json({ user });
+  });
+};
+
+
+
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,13 @@
 // routes user
 
 const express = require("express");
-const {userById, allUsers, getUser} = require("../controllers/user");
+const {userById, allUsers, getUser, updateUser} = require("../controllers/user");
 const router = express.Router();
+const {requireSignin} = require("../controllers/auth");
 
 router.get("/users", allUsers);
-router.get("/user/:userId", getUser);
+router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, updateUser);
 
 // any route containing :userId, our app will first execute userById
 router.param("userId", userById);
